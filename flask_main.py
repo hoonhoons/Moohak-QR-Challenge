d97c8d434b332ce90582cce879bf2ce131222eba
Park, Seonghoon <hoonhoons.park@gmail.com>
2019-05-11 01:32:59 +0900

    commit

--- flask_main.py
+++ flask_main.py
@@ -11,13 +11,28 @@ from flask import Flask, request
 
 this_app = Flask(__name__)
 
+NUM_MAX = 30
+nums = [0] * NUM_MAX
+
 @this_app.route('/')
+@this_app.route('/index')
 def index():
 	return "박성훈의 flask 서버"
 	
-@this_app.route('/hello')
-def hello():
-	return "hello world"
+@this_app.route('/qr')
+def qr():
+    num = request.args.get('num')
+    if not (num >= 0 and num < NUM_MAX):
+        return "Num is out of range"
+    nums[num] += 1
+    
+	return render_template('image.html', num = num)
+
+@this_app.route('/status')
+    return str(nums)
+
+@this_app.route('/init')
+    return "Successfully init!\n" + str(nums)
 	
 if __name__ == '__main__':
 	this_app.run(debug=True, host='0.0.0.0', port=5002, threaded=True)
